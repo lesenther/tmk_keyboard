ed25cc0f25bd7d9dc5995d9e5a74db06026219e1
Logan Esenther <le@berkeley.edu>
2016-02-15 18:28:57 -0800

    Adjusted fn layer, added mousekey layer

--- keyboard/hhkb/keymap_jp_tko.c
+++ keyboard/hhkb/keymap_jp_tko.c
@@ -31,9 +31,28 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
      */
     KEYMAP_JP(GRV, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, INS, DEL, \
               CAPS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PSCR,SLCK,PAUS, UP, TRNS, \
-              TRNS,VOLD,VOLU,MUTE,TRNS,TRNS,PAST,PSLS,HOME,PGUP,LEFT,RGHT,TRNS,PENT, \
-              TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PPLS,PMNS,END, PGDN,DOWN,TRNS,TRNS,PGUP, \
-              TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS,     TRNS,TRNS,TRNS,TRNS,HOME,PGDN,END),
+              MPLY,VOLD,VOLU,MUTE,TRNS,TRNS,PAST,PSLS,HOME,PGUP,LEFT,RGHT,TRNS,PENT, \
+              MYCM,TRNS,TRNS,TRNS,TRNS,TRNS,PPLS,PMNS,END, PGDN,DOWN,WBAK,PGUP,WFWD, \
+              TRNS,CALC,MAIL,TRNS,TRNS,  TRNS,   TRNS,TRNS,TRNS,TRNS,HOME,PGDN,END),
+
+    /* Layer 2: Mousekey mode
+     * ,-----------------------------------------------------------.
+     * |Pwr| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|Ins|Bsp|
+     * |-----------------------------------------------------------|
+     * |Tab  |   |   |   |   |   |   |   |MwL|MwD|MwU|MwR|   |     |
+     * |------------------------------------------------------`    |
+     * |Contro|VoD|VoU|Mut|   |   |   |   |McL|McD|McU|McR|   |    |
+     * |-----------------------------------------------------------|
+     * |Shift  |   |   |   |   |   |   |Mb3|Mb2|Mb1|Mb4|Mb5|   |   |
+     * |-----------------------------------------------------------|
+     * |   ||   |   |   |   |         |   |   |   |   ||   |   |   |
+     * `-----------------------------------------------------------'
+     */
+    KEYMAP_JP(ESC, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, INS, BSPC, \
+              TAB, TRNS,TRNS,TRNS,TRNS,TRNS,WH_L,WH_D,WH_U,WH_R,TRNS,TRNS,TRNS, \
+              LCTL,TRNS,ACL0,ACL1,ACL2,TRNS,MS_L,MS_D,MS_U,MS_R,SCLN,QUOT,BSLS,ENT, \
+              LSFT,TRNS,TRNS,TRNS,TRNS,BTN3,BTN2,BTN1,BTN4,BTN5,SLSH,RSFT,TRNS,TRNS, \
+              FN0, LALT,LGUI,HOME,END,   BTN1,   PGUP,PGDN,RALT,DEL,LEFT,DOWN,RGHT),
 };
 
 
@@ -47,4 +66,5 @@ const uint16_t fn_actions[] __attribute__ ((section (".keymap.fn_actions"))) = {
 const uint16_t fn_actions[] PROGMEM = {
 #endif
     [0] = ACTION_LAYER_MOMENTARY(1),
+    [1] = ACTION_LAYER_TOGGLE(2),
 };
